47026a470d346e75008ec98461c22f8887976f69
froschi95 <60326840+froschi95@users.noreply.github.com>
2022-08-04 12:29:58 +0100

    Update file

--- instruct.c
+++ instruct.c
@@ -1,27 +1,27 @@
 #include "monty.h"
-
 /**
  * instruct_push - pushes a data/number to stack
  * @stack: pointer to the top node of stack
  * @line: the current file line number calling instruction
  */
+
 void instruct_push(stack_t **stack, unsigned int line)
 {
-char *str;
-int num;
-str = strtok(NULL, "\n\t\r ");
-if (str == NULL || check_isdigit(str))
-{
-fprintf(stderr, "L%u: usage: push integer\n", line);
-exit(EXIT_FAILURE);
-}
-num = atoi(str);
-if (!add_node(stack, num))
-{
-fprintf(stderr, "Error: malloc failed\n");
-exit(EXIT_FAILURE);
-}
-var.len_stack++;
+    char *str;
+    int num;
+    str = strtok(NULL, "\n\t\r ");
+    if (str == NULL || check_isdigit(str))
+    {
+        fprintf(stderr, "L%u: usage: push integer\n", line);
+        exit(EXIT_FAILURE);
+    }
+    num = atoi(str);
+    if (!add_node(stack, num))
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    var.len_stack++;
 }
 
 /**
@@ -29,18 +29,19 @@ var.len_stack++;
  * @stack: pointer to the top node of stack
  * @line: the current file line number calling instruction
  */
+
 void instruct_pall(stack_t **stack, unsigned int line)
 {
-stack_t *head;
-(void) line;
-head = *stack;
-while (head != NULL)
-{
-printf("%d\n", head->n);
-head = head->next;
-if (head == *stack)
-return;
-}
+    stack_t *head;
+    (void) line;
+    head = *stack;
+    while (head != NULL)
+    {
+        printf("%d\n", head->n);
+        head = head->next;
+        if (head == *stack)
+            return;
+    }
 }
 
 /**
@@ -48,15 +49,16 @@ return;
  * @stack: pointer to the top node of stack
  * @line: the current file line number calling instruction
  */
+
 void instruct_pint(stack_t **stack, unsigned int line)
 {
-stack_t *head = *stack;
-if (var.len_stack == 0)
-{
-fprintf(stderr, "L%u: can't pint, stack empty\n", line);
-exit(EXIT_FAILURE);
-}
-printf("%d\n", head->n);
+    stack_t *head = *stack;
+    if (var.len_stack == 0)
+    {
+        fprintf(stderr, "L%u: can't pint, stack empty\n", line);
+        exit(EXIT_FAILURE);
+    }
+    printf("%d\n", head->n);
 }
 
 /**
@@ -64,22 +66,23 @@ printf("%d\n", head->n);
  * @stack: pointer to the top node of stack
  * @line: the current file line number calling instruction
  */
+
 void instruct_pop(stack_t **stack, unsigned int line)
 {
-stack_t *pop = *stack;
-if (var.len_stack == 0)
-{
-fprintf(stderr, "L%u: can't pop an empty stack\n", line);
-exit(EXIT_FAILURE);
-}
-(*stack)->next->prev = (*stack)->prev;
-(*stack)->prev->next = (*stack)->next;
-if (var.len_stack != 1)
-*stack = (*stack)->next;
-else
-*stack = NULL;
-free(pop);
-var.len_stack--;
+    stack_t *pop = *stack;
+    if (var.len_stack == 0)
+    {
+        fprintf(stderr, "L%u: can't pop an empty stack\n", line);
+        exit(EXIT_FAILURE);
+    }
+    (*stack)->next->prev = (*stack)->prev;
+    (*stack)->prev->next = (*stack)->next;
+    if (var.len_stack != 1)
+        *stack = (*stack)->next;
+    else
+        *stack = NULL;
+        free(pop);
+        var.len_stack--;
 }
 
 /**
@@ -87,25 +90,26 @@ var.len_stack--;
  * @stack: pointer to the top node of stack
  * @line: the current file line number calling instruction
  */
+
 void instruct_swap(stack_t **stack, unsigned int line __attribute__ ((unused)))
 {
-stack_t *tmp;
-if (var.len_stack < 2)
-{
-fprintf(stderr, "L%u: can't swap, stack too short\n", line);
-exit(EXIT_FAILURE);
-}
-if (var.len_stack == 2)
-{
-*stack = (*stack)->next;
-return;
-}
-tmp = (*stack)->next;
-tmp->prev = (*stack)->prev;
-(*stack)->prev->next = tmp;
-(*stack)->prev = tmp;
-(*stack)->next = tmp->next;
-tmp->next->prev = *stack;
-tmp->next = *stack;
-*stack = tmp;
+    stack_t *tmp;
+    if (var.len_stack < 2)
+    {
+        fprintf(stderr, "L%u: can't swap, stack too short\n", line);
+        exit(EXIT_FAILURE);
+    }
+    if (var.len_stack == 2)
+    {
+        *stack = (*stack)->next;
+        return;
+    }
+    tmp = (*stack)->next;
+    tmp->prev = (*stack)->prev;
+    (*stack)->prev->next = tmp;
+    (*stack)->prev = tmp;
+    (*stack)->next = tmp->next;
+    tmp->next->prev = *stack;
+    tmp->next = *stack;
+    *stack = tmp;
 }
